220aa43cc08075b9cf382bb0bf6c12057661c06e
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-11 18:16:18 -0300

    [WORKS] Criação do componente para renderização de icones no workspace

---
+++ modules/workspace/src/app/shared/components/icon/icon.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Component({
+  selector: 'workspace-icon',
+  template: `<div [innerHTML]="sanitizedSvg"></div>`,
+})
+export class IconComponent implements OnInit{
+  @Input({required: false})
+  forcedClass = 'w-6 h-6'
+
+  @Input({required: true})
+  icon:string = ''
+
+  sanitizedSvg: SafeHtml = '';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    const svgWithClass = this.icon?.replace('<svg', `<svg class="${this.forcedClass}"`).replace("style=\"width:var(--ng-icon__size, 1em);height:var(--ng-icon__size, 1em)\"","")
+    this.sanitizedSvg = this.sanitizer.bypassSecurityTrustHtml(svgWithClass ?? '');
+  }
+
+}

c1f206af2f0d268b973c633e6c9948f83c81c5cd
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-11 21:24:19 -0300

    [Tailwind] Criação da lib com o preset

--- modules/workspace/tailwind.config.js
+++ modules/workspace/tailwind.config.js
@@ -1,30 +1,11 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
+const sharedTailwindConfig = require('../../libs/tailwind-preset/tailwind.config');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [sharedTailwindConfig],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#eff6ff',
-          100: '#dbeafe',
-          200: '#bfdbfe',
-          300: '#93c5fd',
-          400: '#60a5fa',
-          500: '#3b82f6',
-          600: '#2563eb',
-          700: '#1d4ed8',
-          800: '#1e40af',
-          900: '#1e3a8a',
-          950: '#172554',
-        },
-      },
-    },
-  },
-  plugins: [require('flowbite/plugin')],
 };

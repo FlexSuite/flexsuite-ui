cb59e1052a7d1dfc9212947a3115fae59919bef7
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-11 23:13:39 -0300

    [WORKS] navbar Remoção de console log

--- modules/workspace/src/app/shared/components/navbar/components/apps/apps.component.ts
+++ modules/workspace/src/app/shared/components/navbar/components/apps/apps.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppMenu } from '@flexsuite/core/constants';
-import { FlexSuiteIcons, appIcon } from '@flexsuite/core/icons';
+import { appIcon } from '@flexsuite/core/icons';
 
 @Component({
   selector: 'workspace-navbar-apps',
   templateUrl: './apps.component.html',
 })
-export class AppsComponent implements OnInit{
+export class AppsComponent{
   modules = AppMenu.sort((a,b) => a.label.localeCompare(b.label))
   icon = appIcon
-
-  ngOnInit(): void {
-    console.log(this.modules)
-    console.log(FlexSuiteIcons.getModuleIcon(this.modules[0]?.label))
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  
   adjustLabel(label: string): string {
     return label.split("_")[0]
     .replace("Relatorios","Relatórios")

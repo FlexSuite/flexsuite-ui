b22df363bc44343197c503f561dd99d3b190cb8e
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-13 23:19:31 -0300

    [WORKS] Ajuste no dropdown dos modulos

--- modules/workspace/src/app/shared/components/navbar/components/apps/apps.component.ts
+++ modules/workspace/src/app/shared/components/navbar/components/apps/apps.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppMenu } from '@flexsuite/core/constants';
 import { appIcon } from '@flexsuite/core/icons';
 import { IAppItem } from '@flexsuite/core/interfaces';
 import { FlexSuiteNavigationService } from '@flexsuite/foundation/services';
+import { Dropdown, DropdownInterface, DropdownOptions } from 'flowbite';
 @Component({
   selector: 'workspace-navbar-apps',
   templateUrl: './apps.component.html',
 })
-export class AppsComponent{
+export class AppsComponent implements OnInit{
+  dropdown: DropdownInterface | undefined
   modules = AppMenu.sort((a,b) => a.label.localeCompare(b.label))
   icon = appIcon
 
@@ -17,9 +19,37 @@ export class AppsComponent{
 
   }
 
+  ngOnInit(): void {
+    const $dropdown = document.getElementById('modules-dropdown')
+    const $toggle = document.getElementById('modules-dropdown-toggle')
+
+    const configurations: DropdownOptions = {
+      placement: 'bottom',
+      triggerType: 'click',
+      offsetSkidding: 0,
+      offsetDistance: 10,
+      delay: 300,
+      ignoreClickOutsideClass: false,
+    }
+
+    const instanceOptions = {
+      id: 'moduleDropdown',
+      override: true
+    };
+
+    this.dropdown = new Dropdown($dropdown, $toggle, configurations, instanceOptions);
+    this.dropdown?.init()
+  }
+
+  showDropdown(){
+    this.dropdown?.show()
+  }
+
   navigate(module: IAppItem){
     if(!module.path) return console.error("Module without path")
     this.navigator.navigate(module.path)
+
+    this.dropdown?.toggle()
   }
 
   adjustLabel(label: string): string {

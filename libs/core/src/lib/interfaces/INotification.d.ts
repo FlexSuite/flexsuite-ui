40c572be8dff8e235fc166cbf56daa8325447606
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-25 21:50:11 -0300

    [ WORC-3 ] Criação das interfaces necessárias para o sistema de notificações

---
+++ libs/core/src/lib/interfaces/INotification.d.ts
@@ -0,0 +1,22 @@
+import { NotificationType } from './../enums/NotificationType';
+
+export interface INotification {
+    id: string;
+    title: string;
+    description: string;
+    type: NotificationType;
+    authorId?: string;
+    author?: string;
+    read: boolean;
+    viewedAt?: Date;
+    createdAt: Date;
+}
+
+export interface NotificationSendProps {
+    title: string,
+    description: string,
+    type?: NotificationType,
+    authorId?: string,
+    author?: string,
+    read?: boolean ,
+  }

4e334367f307521162b3962c77c807da870b649a
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-25 21:50:22 -0300

    [ WORC-3 ] Criação do enum de tipos de notificações

---
+++ libs/core/src/lib/enums/NotificationType.ts
@@ -0,0 +1,6 @@
+export enum NotificationType {
+    DEFAULT, //System notifications
+    PUSH, //Push notifications
+    ALERT, //Alert notifications
+    ERROR, //Error notifications
+}
--- libs/core/src/lib/enums/index.ts
+++ libs/core/src/lib/enums/index.ts
@@ -1,1 +1,2 @@
 export * from './FlexSuiteNavigation';
+export * from './NotificationType';

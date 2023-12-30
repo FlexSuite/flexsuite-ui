2b84cf39053d97abf4960bf6b3ec7942b6d1e46d
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-30 13:23:52 -0300

    [ WORKS-11 ] Icones de notificação

--- libs/core/src/lib/enums/NotificationType.ts
+++ libs/core/src/lib/enums/NotificationType.ts
@@ -1,6 +1,15 @@
+import { NotificationAlertIcon, NotificationErrorIcon, NotificationInfoIcon, NotificationSuccessIcon } from "../icons";
+
 export enum NotificationType {
     DEFAULT, //System notifications
     PUSH, //Push notifications
     ALERT, //Alert notifications
     ERROR, //Error notifications
+}
+
+export enum NotificationIcon {
+    SUCCESS = NotificationSuccessIcon,
+    ALERT = NotificationAlertIcon,
+    ERROR = NotificationErrorIcon,
+    INFO = NotificationInfoIcon,
 }

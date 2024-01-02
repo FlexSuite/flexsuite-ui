a2b05bb0ce4082b474b6ceac783ff03318d7d470
LucaoMendes <lucas1999mendes@gmail.com>
2024-01-02 13:42:48 -0300

    [ SECAC-3 ] Integração breadcumb na homepage workspace e ajuste no padding

--- modules/workspace/src/app/core/core.module.ts
+++ modules/workspace/src/app/core/core.module.ts
@@ -3,12 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { NotificationService } from '@flexsuite/foundation';
+import { FoundationCommonModule, NotificationService } from '@flexsuite/foundation';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule, FoundationCommonModule],
   providers: [NotificationService],
   exports: [HomeComponent, LoginComponent],
 })

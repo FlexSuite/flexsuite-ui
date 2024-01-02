ff972e0deda7955567bf4c9fe6da0c74ecfd9aba
LucaoMendes <lucas1999mendes@gmail.com>
2024-01-02 00:23:27 -0300

    [ SECAS ] Criação das rotas da SECAP-10 e SECAP-11

--- modules/secas/src/app/remote-entry/entry.routes.ts
+++ modules/secas/src/app/remote-entry/entry.routes.ts
@@ -1,6 +1,13 @@
 import { Route } from '@angular/router';
 import { RemoteEntryComponent } from './entry.component';
+import { ProviderComponent } from '../core/provider/provider.component';
+import { UsersComponent } from '../core/users/users.component';
+import { FlexSuiteModuleRoutes } from '@flexsuite/core/constants';
+
+const routes = FlexSuiteModuleRoutes.Seguranca_e_Controle_de_Acesso
 
 export const remoteRoutes: Route[] = [
   { path: '', component: RemoteEntryComponent },
+  { path: routes.Prestadores, component: ProviderComponent },
+  { path: routes.Usuarios, component: UsersComponent}
 ];

af2f2c45dc70aa7b56024c6b102438ff137917ff
LucaoMendes <lucas1999mendes@gmail.com>
2024-01-02 13:43:55 -0300

    [ SECAP-15 ] Alteração do roteamento para homepage

--- modules/secas/src/app/remote-entry/entry.routes.ts
+++ modules/secas/src/app/remote-entry/entry.routes.ts
@@ -1,13 +1,13 @@
 import { Route } from '@angular/router';
-import { RemoteEntryComponent } from './entry.component';
 import { ProviderComponent } from '../core/provider/provider.component';
 import { UsersComponent } from '../core/users/users.component';
 import { FlexSuiteModuleRoutes } from '@flexsuite/core/constants';
+import { HomeComponent } from '../core/home/home.component';
 
 const routes = FlexSuiteModuleRoutes.Seguranca_e_Controle_de_Acesso
 
 export const remoteRoutes: Route[] = [
-  { path: '', component: RemoteEntryComponent },
+  { path: '', component: HomeComponent },
   { path: routes.Prestadores, component: ProviderComponent },
   { path: routes.Usuarios, component: UsersComponent}
 ];
--- modules/secas/src/bootstrap.ts
+++ modules/secas/src/bootstrap.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { RemoteEntryComponent } from './app/remote-entry/entry.component';
+import { HomeComponent } from './app/core/home/home.component';
 
-bootstrapApplication(RemoteEntryComponent, appConfig).catch((err) =>
+bootstrapApplication(HomeComponent, appConfig).catch((err) =>
   console.error(err)
 );

53e31ff57c8770a4d88706b1ce1f2b116f45fa63
LucaoMendes <lucas1999mendes@gmail.com>
2024-01-02 00:23:14 -0300

    [ SECAS ] Carregamento do modulo

--- modules/secas/src/app/remote-entry/entry.component.ts
+++ modules/secas/src/app/remote-entry/entry.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { LoaderService } from '@flexsuite/foundation/services';
+import { LoaderService } from '@flexsuite/foundation';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NxWelcomeComponent],
+  imports: [CommonModule],
   selector: 'secas-secas-entry',
-  template: `<secas-nx-welcome></secas-nx-welcome>`,
+  template: `<div>Secas Module</div>`,
 })
-export class RemoteEntryComponent {
+export class RemoteEntryComponent{
+  loading = false
   constructor(
     private loader: LoaderService
   ) {
-    setTimeout(()=>this.loader.hide(),2500)
+    this.loader.isLoading.subscribe((loading) => {
+      this.loading = loading
+    })
+
+    this.loader.show()
+
+    setTimeout(()=>{
+      this.loader.hide()
+    },1500)
   }
 }
